4d137e0bfc3f5f422d3d057d143d214758ae78a0
ecklf <ecklf@icloud.com>
2021-12-05 14:18:47 +0100

    style(day5): fmt

--- src/day5.rs
+++ src/day5.rs
@@ -1,6 +1,6 @@
 use aoc_runner_derive::{aoc, aoc_generator};
-use std::ops::{RangeInclusive};
 use std::collections::HashMap;
+use std::ops::RangeInclusive;
 
 const OVERLAP: u32 = 2;
 
@@ -62,10 +62,7 @@ fn input_generator(input: &str) -> Vec<Line> {
                         .map(|p| p.parse::<i32>().unwrap())
                         .collect::<Vec<_>>();
 
-                    Point {
-                        x: p[0],
-                        y: p[1],
-                    }
+                    Point { x: p[0], y: p[1] }
                 })
                 .collect::<Vec<_>>();
 
@@ -81,73 +78,69 @@ fn input_generator(input: &str) -> Vec<Line> {
 pub fn filter_overlaps(lines: &Vec<Line>, overlap_count: u32, with_diagonals: bool) -> usize {
     let mut vents_map = HashMap::<(i32, i32), u32>::new();
 
-    let filtered_lines = lines.into_iter().filter(|l|
-        {
-            match l.pitch() {
-                None => true,
-                Some(v) => match v {
-                    0 => true,
-                    1 | -1 => with_diagonals,
-                    _ => false
-                },
-            }
-        }
-    ).collect::<Vec<_>>();
+    let filtered_lines = lines
+        .into_iter()
+        .filter(|l| match l.pitch() {
+            None => true,
+            Some(v) => match v {
+                0 => true,
+                1 | -1 => with_diagonals,
+                _ => false,
+            },
+        })
+        .collect::<Vec<_>>();
 
-    let mut upsert_vents_map = |x: i32, y: i32| {
-        match vents_map.get_mut(&(x, y)) {
-            Some(m) => {
-                *m += 1
-            }
-            None => {
-                vents_map.insert((x, y), 1);
-            }
+    let mut upsert_vents_map = |x: i32, y: i32| match vents_map.get_mut(&(x, y)) {
+        Some(m) => *m += 1,
+        None => {
+            vents_map.insert((x, y), 1);
         }
     };
 
-    filtered_lines.into_iter().for_each(|l| {
-        match l.pitch() {
-            None => {
-                let x = l.start.x;
-                let reverse = l.start.y > l.end.y;
-                let y_range = l.get_y_range(reverse);
+    filtered_lines.into_iter().for_each(|l| match l.pitch() {
+        None => {
+            let x = l.start.x;
+            let reverse = l.start.y > l.end.y;
+            let y_range = l.get_y_range(reverse);
 
-                y_range.for_each(|y| {
-                    upsert_vents_map(x, y);
-                });
-            }
-            Some(0) => {
-                let y = l.start.y;
-                let reverse = l.start.x > l.end.x;
-                let x_range = l.get_x_range(reverse);
+            y_range.for_each(|y| {
+                upsert_vents_map(x, y);
+            });
+        }
+        Some(0) => {
+            let y = l.start.y;
+            let reverse = l.start.x > l.end.x;
+            let x_range = l.get_x_range(reverse);
 
-                x_range.for_each(|x| {
-                    upsert_vents_map(x, y);
-                });
-            }
-            Some(p) => {
-                let reverse = l.start.x > l.end.x;
-                let mut y = match reverse {
-                    false => l.start.y,
-                    true => l.end.y,
-                };
+            x_range.for_each(|x| {
+                upsert_vents_map(x, y);
+            });
+        }
+        Some(p) => {
+            let reverse = l.start.x > l.end.x;
+            let mut y = match reverse {
+                false => l.start.y,
+                true => l.end.y,
+            };
 
-                let x_range = match reverse {
-                    false => l.start.x..=l.end.x,
-                    true => l.end.x..=l.start.x
-                };
+            let x_range = match reverse {
+                false => l.start.x..=l.end.x,
+                true => l.end.x..=l.start.x,
+            };
 
-                x_range.for_each(|x| {
-                    upsert_vents_map(x, y);
-                    y += p;
-                });
-            }
+            x_range.for_each(|x| {
+                upsert_vents_map(x, y);
+                y += p;
+            });
         }
     });
 
-    vents_map.into_iter().filter(|(_, val)| val >= &overlap_count).collect::<Vec<_>>().len()
+    vents_map
+        .into_iter()
+        .filter(|(_, val)| val >= &overlap_count)
+        .collect::<Vec<_>>()
+        .len()
 }
-
 
 #[aoc(day5, part1)]
 pub fn part_one(input: &Vec<Line>) -> usize {
